26f102ac827fe9b66a7aeb4c552c8d801ce5fa56
davidjame1 <jbdavidjames@gmail.com>
2023-10-30 10:51:17 +0530

    created assignment in Test1

---
+++ Assignment 1.py
@@ -0,0 +1,8 @@
+#Program to Input two values and output its Add and Product
+print("User inputs") 
+a = int(input ("Enter the Value of a:"))
+b = int(input ("Enter the value of b:"))
+Add= a+b
+Product = a*b
+print("Addition of a and b :",Add)
+print("Multiplication of a and b :",Product)
