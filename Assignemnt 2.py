21981436332fceb6b59365fe73657edcdb124339
davidjame1 <jbdavidjames@gmail.com>
2023-10-31 19:28:22 +0530

    Created new file

---
+++ Assignemnt 2.py
@@ -0,0 +1,7 @@
+#string assignment
+
+print("user inputs")
+Name=input("Enter your Name:")
+Age=input("Enter your age:")
+Favorite_Color=input("Enter your Favorite Color")
+print(Name Age Favorite_Color)
