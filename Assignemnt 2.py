fa4653b62e49cbfcf8fe1c8b7d2c7a60ff53e9ca
davidjame1 <jbdavidjames@gmail.com>
2023-10-31 22:54:15 +0530

    Modified

--- Assignemnt 2.py
+++ Assignemnt 2.py
@@ -8,5 +8,5 @@ print("")
 print("")
 output= "Hello,my name is {0}.I am {1} years old.My Favourite color is {2}"
 print ("output:")
-X= Name.strip().lower().split()
-print(output.format(X[0],Age.strip(),Favorite_Color.strip().upper()))
+X= Name.strip().upper().split()
+print(output.format(X[0],Age.strip(),Favorite_Color.strip().lower()))
