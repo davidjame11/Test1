d6e2b12a141008ffa56f557a8861580ddcaa1b56
davidjame1 <jbdavidjames@gmail.com>
2023-10-31 22:34:07 +0530

    created new assignment2

--- Assignemnt 2.py
+++ Assignemnt 2.py
@@ -1,7 +1,12 @@
-#string assignment
+#string assignment DAVID JAMES J
 
 print("user inputs")
 Name=input("Enter your Name:")
 Age=input("Enter your age:")
-Favorite_Color=input("Enter your Favorite Color")
-print(Name Age Favorite_Color)
+Favorite_Color=input("Enter your Favorite Color:")
+print("")
+print("")
+output= "Hello,my name is {0}.I am {1} years old.My Favourite color is {2}"
+print ("output:")
+X= Name.strip().lower().split()
+print(output.format(X[0],Age.strip(),Favorite_Color.strip().upper()))
